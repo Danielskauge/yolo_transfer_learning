1872c922372c8e1f5c74581f510338bd7cea9380
Daniel Rosmæl Skauge <2001leinad@gmail.com>
2021-11-18 10:55:46 +0100

    commits the initial files

---
+++ train_yolo_test.py
@@ -0,0 +1,39 @@
+import keras
+import cv2
+import matplotlib
+import numpy as np
+from model.yolo_model import YOLO
+
+def process_image(img):
+    """Resize, reduce and expand image.
+
+    # Argument:
+        img: original image.
+
+    # Returns
+        image: ndarray(64, 64, 3), processed image.
+    """
+    image = cv2.resize(img, (416, 416),
+                       interpolation=cv2.INTER_CUBIC)
+    image = np.array(image, dtype='float32')
+    image /= 255.
+    image = np.expand_dims(image, axis=0)
+
+    return image
+
+def get_classes(file):
+    """Get classes name.
+
+    # Argument:
+        file: classes name for database.
+
+    # Returns
+        class_names: List, classes name.
+
+    """
+    with open(file) as f:
+        class_names = f.readlines()
+    class_names = [c.strip() for c in class_names]
+
+    return class_names
+    
